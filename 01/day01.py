6ace3372f8df374bbc70c004699e4317b111125f
Ales <Ales Podolnik>
2021-12-01 11:37:49 +0100

    Day 1

---
+++ 01/day01.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+with open('input.txt', mode='r') as f:
+    data = np.array([int(l.strip()) for l in f.readlines()])
+    
+
+# part 1
+differences = data[1:] - data[0:-1]
+num_inc = sum(differences > 0)
+print(num_inc)
+
+# part 2
+sums_3 = np.array([sum(data[i:i+3]) for i in range(len(data)-2)])
+differences = sums_3[1:] - sums_3[0:-1]
+num_inc = sum(differences > 0)
+print(num_inc)
