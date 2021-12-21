fd85689e940db90a34f1f20b3735f56f0df39c5e
Aleš Podolník <ales@podolnik.me>
2021-12-21 18:02:19 +0100

    Day 21, part 2

--- 21/day21.py
+++ 21/day21.py
@@ -7,7 +7,7 @@ Created on Tue Dec 21 10:00:15 2021
 
 from pprint import pprint
 
-with open('test.txt', mode='r') as f:
+with open('input.txt', mode='r') as f:
     positions = [int(l.strip().split(' ')[-1]) for l in f.readlines()]
     
 def roll(n):
@@ -44,37 +44,58 @@ pos_a = positions[0] - 1
 pos_b = positions[1] - 1
 print(play_1(pos_a, pos_b))
 
-def play_2(pos_a, pos_b):
-    wins = {'a': 0, 'b': 0}
-    
-    other = {'a': 'b', 'b': 'a'}
-    
-    states = [{'turn': 'a', 'a': (pos_a, 0), 'b': (pos_b, 0), 'mul': 1}]
+
+def sums_tot(n):
+    all_sums = []
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            for k in range(1, n + 1):
+                all_sums.append(i+j+k)
+    return all_sums
+
+def sums(n):
+    all_sums = sums_tot(n)
+    uniques = list(set(all_sums))
+    return uniques, [len([s for s in all_sums if s == u]) for u in uniques]
+
+all_sums, multiplicities = sums(3)
+
+cache = {}
+
+def play_r(state):
+    if state in cache:
+        return cache[state]
+    wins = [0, 0]        
+    states = [state]
     
     while len(states) > 0:
         state = states.pop(0)
-        pprint(state)
+        # pprint(state)
         
-        for r in range(1, 4):
-            pid = state['turn']
-            oid = other[pid]
+        for r, m in zip(all_sums, multiplicities):
+            pid = state[0]
             
-            pos = (state[pid][0] + r) % 10
-            sco = state[pid][1] + pos + 1
+            pos = (state[pid * 2 + 1] + r) % 10
+            sco = state[pid * 2 + 2] + pos + 1
+            mul = state[5] * m
             
             if sco >= 21:
-                wins[pid] += 1
-                print(f'{pid} wins')
+                wins[pid] += mul
             else:
-                new_state = {'turn': oid, pid: (pos, sco), oid: state[oid], 'mul': state['mul'] + 1}
-                states.append(new_state)
+                next_state = ()
+                if pid == 0:
+                    next_state = (1, pos, sco, state[3], state[4], mul)
+                else:
+                    next_state = (0, state[1], state[2], pos, sco, mul)
+                    
+                r_wins = play_r(next_state)
+                wins[0] += r_wins[0]
+                wins[1] += r_wins[1]
+    
+    if state not in cache:
+        cache[state] = wins
         
     return wins
 
-pos_a = positions[0] - 1
-pos_b = positions[1] - 1
-wins = play_2(pos_a, pos_b)
-print(max(wins.values()))
-            
-        
-        
+wins = play_r((0, pos_a, 0, pos_b, 0, 1))
+print(max(wins))
